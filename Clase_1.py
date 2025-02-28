950e12c373243bb0de254adfedea2bdc3c972961
rastor65 <rdamianquintero@uniguajira.edu.co>
2025-02-28 13:06:24 -0500

    IAN

--- Clase_1.py
+++
@@ -1,7 +0,0 @@
-print ("Bienvenido al programa de gestión de usuarios!")
-nombre = input("Ingrese su nombre: ")
-print (f"Hola, {nombre}!")
-edad = int(input("Ingrese su edad: "))
-print(f"{nombre}, tienes {edad} años.")
-año_nacimiento = 2025 - edad
-print(f"{nombre}, naciste en el año {año_nacimiento}.")
