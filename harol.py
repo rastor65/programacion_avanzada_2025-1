4906615da009988d11d788502bb1bd816ddc704f
harol <harolpolomeza@gmail.com>
2025-02-28 13:32:12 -0500

    Update harol.py

--- harol.py
+++ harol.py
@@ -1,1 +1,9 @@
-print("hola mundo")
+presupuestos = float(input("cuanto es su presupuesto"))
+verduras = float(input("ingrse cuanto costo las futas "))
+carnes = float(input("ingrse cuanto costo las futas "))
+especias = float(input("ingrse cuanto costo las futas "))
+presupuesto_total = verduras + carnes + especias
+if presupuesto_total <= presupuestos:
+    print("usted puede comprar todo eso")
+else:
+    print("usted es pobre no puede comprar todo eso")
