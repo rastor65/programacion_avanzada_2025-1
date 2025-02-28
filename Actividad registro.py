a90e51ba43ba8b3b6780623054cbd73cdd3cb1a5
PandaEgames2 <161091704+PandaEgames2@users.noreply.github.com>
2025-02-28 14:14:18 -0500

    actividad

--- Actividad registro.py
+++ Actividad registro.py
@@ -1,1 +1,6 @@
-
+print("Bienvenido al Bienestar")
+nombre_usu = str(input("Porfavor ingrese su nombre de usuario"))
+if len(nombre_usu) > 3 and len(nombre_usu) < 8:
+    print(f"¡Hola, {nombre_usu.upper()}!")
+else:
+    print("El nombre debe tener entre 4 y 7 caracteres como maximo")
