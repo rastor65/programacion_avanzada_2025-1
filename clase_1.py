1c59ebb78c0cd330c6e6b3d5f9b6cc3c9dad6857
rastor65 <rdamianquintero@uniguajira.edu.co>
2025-03-07 13:14:21 -0500

    Actulizar

--- clase_1.py
+++ clase_1.py
@@ -1,11 +1,5 @@
-#Paquete de software
-valor_licen = float(input("INgrese el valor de las licensia de software"))
-presupuesto_escuela = float(input("Ingrese el prespuesto que tiene para las licencias"))
-licencia_comprada =float(input("Ingrese la cantidad de licencias que desea comprar"))
-total_de_compra = valor_licen * licencia_comprada
-if total_de_compra == presupuesto_escuela or total_de_compra>presupuesto_escuela:
-    print("No puede comprar licencias adicionales")
-elif total_de_compra<presupuesto_escuela:
-    presupuesto_restante = presupuesto_escuela - total_de_compra
-    licencias_adicionales = presupuesto_restante // valor_licen
-    print(f"Se puede comprar {int(licencias_adicionales)} licencias adicionales")
+print ("Bienvenido al programa de gestión de usuarios!")
+nombre = input("Ingrese su nombre: ")
+print (f"Hola, {nombre}!")
+edad = int(input("Ingrese su edad: "))
+print(f"{nombre}, tienes {edad} años.")
