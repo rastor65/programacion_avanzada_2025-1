9fe6ab5b69cc726e4acaa6750be7b6f01cc5c617
Paulo080605 <pacesar2005@gmail.com>
2025-03-07 14:39:15 -0500

    Create registronombre.py

---
+++ registronombre.py
@@ -0,0 +1,6 @@
+nombre = str(input("ingrese el nombre :"))
+if len(nombre) > 3 and  len(nombre) < 8:
+     print (f"bienvenido,  {nombre.upper()} ha sido permitido")
+else :
+     print ("su nombre no es permitido porque no cumplue con las condiciones de los caracteres ")
+
