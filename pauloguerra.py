45c96f1eb9a4b0b0bd08f6df07deeb3c6da938bc
Paulo080605 <pacesar2005@gmail.com>
2025-03-07 13:42:18 -0500

    clasepaulo

---
+++ pauloguerra.py
@@ -0,0 +1,8 @@
+print(f"estamos vendiendo paquetes de software por licencia a "(preciolicencia))
+
+presupuesto = float(input("ingrese el presupuesto: "))
+licencias = int(input("ingrese la cantidad de licencias: "))
+
+preciolicencia = 100
+
+
