4662beb6af7a7c8ef4897b706666de6dee1a75cc
JonathanRedondoU1 <jonathanremi1006@gmail.com>
2025-03-07 14:52:55 -0500

    Nuevosmetodos

---
+++ ClasePAPresencial_3.py
@@ -0,0 +1,9 @@
+texto = input("Ingresa tu articulo: ")
+
+texto_modificado = texto.replace("tecnologia antigua", "tecnologia de punta")
+
+print (texto_modificado)
+
+textoSplit= texto.split()
+
+print(textoSplit)
