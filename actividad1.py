54aa516aab83d24b917e20850093fa96580ec06f
ANDRESMOSCOTE7 <andrehassidvega7@gmail.com>
2025-03-07 14:37:26 -0500

    .

--- actividad1.py
+++ actividad1.py
@@ -1,1 +1,5 @@
-print("hola mundo")
+nombre = input("Ingrese su nombre: ")
+if len(nombre) < 3 or len(nombre) > 8:
+    print("Nombre no valido. Debe tener entre 3-8 caracteres.")
+else: 
+    print(f"Hola, {nombre.upper()}.")
