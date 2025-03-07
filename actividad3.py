2c24b092fe16068fe1437be3be73bf4b8128a107
AngieR02 <anparoro02@gmail.com>
2025-03-07 14:56:54 -0500

    Create actividad3.py

---
+++ actividad3.py
@@ -0,0 +1,5 @@
+estudiantes = float (input("ingrese la cantidad de estudiantes que hay"))
+equipos = float (input("ingrese la cantidad de equios"))
+
+estudiante_por_grupo = estudiantes // equipos
+print (f"se pueden formar {estudiante_por_grupo} equipos")
