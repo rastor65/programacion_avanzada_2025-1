387d4abb48160191b00b6ff2ed643933da19828e
harol <harolpolomeza@gmail.com>
2025-03-07 14:10:14 -0500

    harol

--- harol.py
+++ harol.py
@@ -1,6 +1,10 @@
-nombre = str(input("diga su nombre "))
-if len(nombre) < 3 or len(nombre) > 8:
-    print("el nombre no es valido")
-else: 
-    print(f"hola, {nombre.upper()}")
-
+presupuesto = float(input("cuanto presupuesto tienes: "))
+precioL = float(input("diga el costo de las licencias: "))
+cantL = float(input("cual es la cantidad ? "))
+total = precioL * cantL
+add = presupuesto - total
+if (total <= presupuesto) :
+    print(f"el valor a pagar es de: {total}")
+    print(f"te quedan {add} para comprar")
+elif add < precioL:
+    print(f"no puedes comprar porque te quedan {add}")    
