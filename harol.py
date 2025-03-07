42be38caf4503dfccf3da02a5c0439ee31a99562
harol <harolpolomeza@gmail.com>
2025-03-07 14:15:19 -0500

    Update harol.py

--- harol.py
+++ harol.py
@@ -3,7 +3,9 @@ precioL = float(input("diga el costo de las licencias: "))
 cantL = float(input("cual es la cantidad ? "))
 total = precioL * cantL
 add = presupuesto - total
-if (total <= presupuesto) :
+if total >= presupuesto:
+    print(f"no puedes comprar, no tienes plata ")
+elif (total <= presupuesto) :
     print(f"el valor a pagar es de: {total}")
     print(f"te quedan {add} para comprar")
 elif add < precioL:
