5c6afc87457bac1c6a06e4c44a52acb06972fbbd
Paulo080605 <pacesar2005@gmail.com>
2025-03-07 19:40:17 -0500

    Create talller1.py

---
+++ talller1.py
@@ -0,0 +1,5 @@
+presupuesto = input( ("ingrese su presupuesto chef :   "))
+vegetales = input ((" cuanto vas a gastar en vegetales :  "))
+carnes = input (("cuantos vas a gastar en carnes :  "))
+espesias = input (("cuantos vas a gastar en especias :  "))
+
