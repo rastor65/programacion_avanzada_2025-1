1154ff08f6e3104c881558cf6f518264782a65f4
SebastianPolo23 <polosebasguillen@gmail.com>
2025-03-09 23:45:08 -0500

    Sebastian Polo
    Sebastian Polo

---
+++ actividad dividir lapices.py
@@ -0,0 +1,7 @@
+num_estudiantes = int(input("Cual es la cantidad de estudiantes que hay en el salon: "))
+num_lapices = 32
+if num_estudiantes > num_lapices:
+    print("No habrá lápices sobrantes, ya que la cantidad de estudiantes excede la cantidad de lápices disponibles.")
+else:
+    lapices_sobrantes = num_lapices % num_estudiantes
+    print(f"Al profesor le sobran {lapices_sobrantes} lápices después de entregar.")
