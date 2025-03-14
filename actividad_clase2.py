3888061ea59de934629d22cc1ba44dd0fa9f8752
Alberto Orozco <albertoorozcog39@gmail.com>
2025-03-14 13:48:51 -0500

    ac2

--- actividad_clase2.py
+++ actividad_clase2.py
@@ -1,3 +1,5 @@
+import sys
+
 def calcular_edad (edad):
     if edad < 0:
         return print("Edad no disponible.")
@@ -6,9 +8,28 @@ def calcular_edad (edad):
     elif edad <= 17:
         return print("Eres un adolecente. Enfocate en tus estudios.")
     elif edad <= 64:
-        return print("Eres un adulto. Ordena tu vida.")
+        return print("Eres un adulto. Ordena tu  vida.")
     else:
         return print("Eres un adulto mayor. Disfruta de tu hogar.")
         
-edad = int(input("Ingrese su edad: "))
-calcular_edad(edad)
+#edad = int(input("Ingrese su edad: "))
+#calcular_edad(edad)
+
+def tablaDeMultiplicar (n, o):
+    if o == 1:
+        for i in range(12):
+            print(f"{n} X {i+1} = {n*(i+1)}")
+    else:
+        lista = ""
+        for i in range(12):
+            lista += f"{n} X {i+1} = {n*(i+1)}\t"
+        print(lista)
+numero = int(input("Ingresa un numero: "))
+if numero < 1:
+    print("Ingresa solo numeros enteros positivos.")
+    sys.exit(0)
+orientacion = int(input("Orientacion: \n1. Vertical. \n2. Horizontal.\n"))
+if orientacion != 1 and orientacion != 2:
+    print("Opcion no disponible (1 o 2).")
+else:   
+    tablaDeMultiplicar(numero, orientacion)
