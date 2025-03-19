14510836c6c7befd3eb8111dc8876b29e36cecc9
JonathanRedondoU1 <jonathanremi1006@gmail.com>
2025-03-18 23:00:11 -0500

    Control de flujo, funciones etc, ejer1

---
+++ Activi2_1.py
@@ -0,0 +1,15 @@
+edad = int(input("Hola!, ingrese su edad por favor: "))
+if edad >0 and  edad<=12:
+    print("Usted es un niño")
+    print("De los niños es el reino de los cielos!")
+if edad >=13 and edad <= 17 :
+    print("Usted es un adolescente")
+    print("Ninguno tenga en poco tu juventud, sino sé ejemlo...")
+if edad >=18 and edad <=64:
+    print("Usted es un adulto")
+    print("Teme a Dios y guarda sus mandamientos; porque esto es el todo del hombre")
+if edad >=65:
+    print("Usted es un adulto mayor")
+    print("vuetros ancianos soñaran sueños,")
+if edad <0 and edad >120:
+    print("Información no valida...")
