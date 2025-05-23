e1657da53070489dbfe0fa4db2919591c365d0be
JonathanRedondoU1 <jonathanremi1006@gmail.com>
2025-05-23 10:35:48 -0500

    Django2

--- mi_proyecto/sistema_moderno/sistema_moderno/settings.py
+++ mi_proyecto/sistema_moderno/sistema_moderno/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'usuarios',
+    'asistencias'
+    'notificaciones',
+    'comportamiento',
+    'notas',
+
 ]
 
 MIDDLEWARE = [
